e30dd46e430ff3d8e42280c0a73d5dce9f8b1584
bouzianes <fcbdreams1991@gmail.com>
2022-07-19 23:06:18 -0400

    sending index

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -20,6 +21,10 @@ app.get('/', (req, res) => {
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
